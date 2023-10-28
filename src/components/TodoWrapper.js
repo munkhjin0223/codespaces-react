fd1fee811a8e7bade8b5e07e722a4726b43dcb3e
Munkhjin <munkhjin0223@users.noreply.github.com>
2023-10-28 06:59:07 +0000

    refactor

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,30 +1,7 @@
 import { useReducer } from "react";
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
-
-export function reducer(todos, action) {
-    switch(action.type) {
-        case 'ADD_TODO': {
-            return [...todos, { id: Math.random().toString(),
-                text: action.payload.text,
-                isCompleted: false,
-                isEditing: false
-            }]
-        }
-        case 'EDIT_TODO': {
-            return todos.map((todo) => (todo.id === action.payload.id ? { ...todo, text: action.payload.text, isEditing: false } : todo))
-        }
-        case 'DELETE_TODO': {
-            return todos.filter((todo) => todo.id !== action.payload.id)
-        }
-        case 'TOGGLE_COMPLETE': {
-            return todos.map((todo) => (todo.id === action.payload.id ? { ...todo, isCompleted: !todo.isCompleted } : todo))
-        }
-        case 'TOGGLE_EDITING': {
-            return todos.map((todo) => (todo.id === action.payload.id ? { ...todo, isEditing: !todo.isEditing } : todo))
-        }
-    }
-}
+import { reducer } from '../reducers/todo'
 
 function TodoWrapper() {
     const [todos, dispatch] = useReducer(reducer, [{
